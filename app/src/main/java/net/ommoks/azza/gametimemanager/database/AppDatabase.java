6a23a81bb77fd52042fb9b1ea84022c583826822
ommokazza <ommokazza@gmail.com>
2022-08-19 10:11:10 +0900

    Upload the first source code #2

---
+++ app/src/main/java/net/ommoks/azza/gametimemanager/database/AppDatabase.java
@@ -0,0 +1,9 @@
+package net.ommoks.azza.gametimemanager.database;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Record.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+    public abstract RecordDao recordDao();
+}
---
+++ app/src/main/java/net/ommoks/azza/gametimemanager/database/RecordDao.java
@@ -0,0 +1,22 @@
+package net.ommoks.azza.gametimemanager.database;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface RecordDao {
+    @Query("SELECT * FROM records")
+    List<Record> getAll();
+
+    @Query("SELECT * FROM records WHERE week_index = :weekIndex")
+    List<Record> getAllWithWeekIndex(int weekIndex);
+
+    @Query("SELECT * FROM records WHERE id = (SELECT MAX(id) FROM records)")
+    Record getLastOne();
+
+    @Insert
+    void insert(Record record);
+}
