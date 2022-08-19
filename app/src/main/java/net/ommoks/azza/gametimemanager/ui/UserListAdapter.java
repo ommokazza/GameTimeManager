93fddeca363a745c78755ff6e2aa66a1584330a3
ommokazza <ommokazza@gmail.com>
2022-08-19 22:04:54 +0900

    Work for decrease/increase buttons

--- app/src/main/java/net/ommoks/azza/gametimemanager/ui/UserListAdapter.java
+++ app/src/main/java/net/ommoks/azza/gametimemanager/ui/UserListAdapter.java
@@ -12,27 +12,47 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.google.android.material.textview.MaterialTextView;
 
 import net.ommoks.azza.gametimemanager.R;
+import net.ommoks.azza.gametimemanager.databinding.UserListItemBinding;
 
 import java.util.ArrayList;
 
 public class UserListAdapter extends RecyclerView.Adapter<UserListAdapter.ViewHolder> {
 
-    final private ArrayList<String> mUserList;
+    private final ArrayList<String> mUserList;
+
+    private static final int TIME_UNIT = 15;
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
 
+        private UserListItemBinding mBinding;
+
         final private MaterialTextView name;
-        private AppCompatButton remove;
+        private AppCompatButton decrease;
         private AppCompatTextView time;
-        private AppCompatButton add;
+        private AppCompatButton increase;
         private AppCompatButton done;
 
         public ViewHolder(View view) {
             super(view);
 
             name = view.findViewById(R.id.name);
-            //TODO: click listener
+            time = view.findViewById(R.id.minutes);
+            decrease = view.findViewById(R.id.decrease);
+            decrease.setOnClickListener(view1 -> calculateTime(-TIME_UNIT));
+            increase = view.findViewById(R.id.increase);
+            increase.setOnClickListener(view1 -> calculateTime(TIME_UNIT));
+            done = view.findViewById(R.id.done);
+            done.setOnClickListener(view12 -> {
+                int minutes = Integer.valueOf(time.getText().toString());
+                time.setText("0");
+                //TODO: call listener to submit time
+            });
+        }
 
+        private void calculateTime(int value) {
+            int minutes = Integer.valueOf(time.getText().toString());
+            minutes += value;
+            time.setText(String.valueOf(minutes));
         }
     }
 
