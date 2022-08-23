fc1f787891b79a4cb669d68435a078f86ab4e198
ommokazza <ommokazza@gmail.com>
2022-08-23 14:58:47 +0900

    Apply fragment toolbar

--- app/src/main/java/net/ommoks/azza/gametimemanager/MainActivity.java
+++ app/src/main/java/net/ommoks/azza/gametimemanager/MainActivity.java
@@ -4,25 +4,11 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import net.ommoks.azza.gametimemanager.ui.MainFragment;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        if (findViewById(R.id.fragment_container) != null) {
-            if (savedInstanceState != null) {
-                return;
-            }
-
-            MainFragment mainFragment = new MainFragment();
-            mainFragment.setArguments(getIntent().getExtras());
-
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.fragment_container, mainFragment).commit();
-        }
     }
 }
